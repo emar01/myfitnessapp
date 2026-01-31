13eec33b28f9b10d23bb2f59c2ebb0a75226b455
emar01 <emil.artursson@gmail.com>
2026-01-31 09:32:50 +0100

    fix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -91,7 +91,6 @@ function ProtectedLayout() {
         <Stack.Screen name="workout/[id]" options={{ headerShown: false }} />
         <Stack.Screen name="workout/log" options={{ headerShown: false }} />
         <Stack.Screen name="program/settings" options={{ headerShown: false }} />
-        <Stack.Screen name="settings/profile" options={{ headerShown: false }} />
         <Stack.Screen name="login" options={{ headerShown: false }} />
       </Stack>
     </ThemeProvider>
